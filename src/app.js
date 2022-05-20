9a019ac414d688ddbc98113248a743039bc44d1c
Marius <marius@example.com>
2022-05-20 09:37:18 +0300

    geting a joke

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { port } = require('./config');
+const norisRouter = require('./routes/norisRoutes');
 const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
+
+// Routes
+app.use('/api/', norisRouter);
 
 app.listen(port, () => console.log('Server listening on port ' + port));
